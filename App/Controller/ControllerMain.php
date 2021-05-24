1e92eb3635f266c3cbe39ff6d03574c369812c12
Vlad Butenko <vladbutenkoan@gmail.com>
2021-05-24 23:47:10 +0300

    Added product mapper

--- App/Controller/ControllerMain.php
+++ App/Controller/ControllerMain.php
@@ -5,15 +5,9 @@ namespace App\Controller;
 
 class ControllerMain extends \Framework\Core\Controller
 {
-    private array $products;
-
-    public function __construct()
-    {
-    }
 
     public function actionIndex()
     {
-
         $this->renderer->render("template_view", null, 'main_view');
     }
 }
