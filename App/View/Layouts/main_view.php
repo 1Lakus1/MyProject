b20397c5bb8dec3975d9ba090decfc07acfe942c
Vlad Butenko <vladbutenkoan@gmail.com>
2021-06-05 11:51:45 +0300

    Added loading info

--- App/View/Layouts/main_view.php
+++ App/View/Layouts/main_view.php
@@ -4,16 +4,7 @@
             <div class="title__text"> HOME PAGE </div>
         </div>
         <div id="menu_list" class="main__list">
-            <?php /*foreach($data as $product){ */?><!--
-            <a class="product" href = "/product?id=<?php /*echo $product->getId(); */?>">
-                <img src="src/images/<?php /*echo $product->getImgName(); */?>" alt="" class="product__img">
-                <div class="product__info">
-                    <div class="product__name"> <?php /*echo $product->getName(); */?></div>
-                    <div class="product__price"> $<?php /*echo $product->getPrice(); */?></div>
-
-                </div>
-            </a>
-            --><?php /*} */?>
+            Loading..
         </div>
     </div>
 </main>
